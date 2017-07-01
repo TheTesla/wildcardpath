2ae9aa69181adf2446e001088e3861d6d3ee5ddb
Stefan Helmert <stefan.helmert@informatik.tu-chemnitz.de>
2017-07-01 17:27:16 +0200

    maybe more security with maximum length append and copy of strings

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <dirent.h>
 #include <string.h>
 
+#define MAX_STR_LEN 512
+
 char* getRootPath(char *rootPath, char *wildPath)
 {
     char c;
     c = 1;
     unsigned pidxend = 0;
-    for(int i=0; c && i < 512; i++){
+    for(int i=0; c && i < MAX_STR_LEN; i++){
       c = wildPath[i];
       rootPath[i] = c;
       if('/' == c){
@@ -15,7 +17,7 @@ char* getRootPath(char *rootPath, char *wildPath)
       }
       if('?' == c){
         rootPath[pidxend] = 0;
-        return &wildPath[pidxend+1];
+        return &wildPath[pidxend+1]; // may be dangerous, bacause reduces maximum string length
       }
     }
     return "";
@@ -25,12 +27,12 @@ char* splitStr(char* str, char delim)
 {
     char c;
     c = 1;
-    for(int i=0; c && i < 512; i++){
-      c = str[i];
-      if(delim == c){
-        str[i] = 0;
-        return &str[i+1];
-      }
+    for(int i=0; c && i < MAX_STR_LEN; i++){
+        c = str[i];
+        if(delim == c){
+            str[i] = 0;
+            return &str[i+1];
+        }
 
     }
     return "";
@@ -41,7 +43,7 @@ int cmpStrWild(char* inputStr, char* wildStr)
     char ci, cw;
     ci = 1;
     cw = 1;
-    for(int i = 0; ci && cw; i++){
+    for(int i = 0; ci && cw && i < MAX_STR_LEN; i++){
         ci = inputStr[i];
         cw = wildStr[i];
         if(ci == cw) continue;
@@ -56,12 +58,10 @@ char* findPathWild(char* foundPath, char* wildPath)
 {
     DIR *dir;
     struct dirent *ent;
-    char found[512];
-    char rootPath[512];
-    char currentLabel[512];
+    char found[MAX_STR_LEN];
+    char rootPath[MAX_STR_LEN];
+    char currentLabel[MAX_STR_LEN];
     char* pathEnd;
-
-
     int cmpResult;
 
 
@@ -70,45 +70,26 @@ char* findPathWild(char* foundPath, char* wildPath)
     }
 
     pathEnd = getRootPath(rootPath, wildPath);
-    strcpy(currentLabel, pathEnd);    
+    strncpy(currentLabel, pathEnd, MAX_STR_LEN - 1);    
     pathEnd = splitStr(currentLabel, '/');    
+    strncat(rootPath, "/", MAX_STR_LEN - strlen(rootPath) - 1);
     // structur:   /root/Path / currentLabel / p?th/E?d/
     //           | rootPath   | currentLabel | pathEnd
 
-    printf("wild = %s\n", wildPath);
-    printf("foundPath = %s\n", foundPath);
-    printf("rootPath = %s\n", rootPath);
-    printf("currentLabel = %s\n", currentLabel);
-    printf("pathEnd = %s\n", pathEnd);
-
-
-
-    //if(0 == rootPath[0]){
-        strcat(rootPath, "/");
-    //}
-    printf("opendir = %s\n", rootPath);
     if ((dir = opendir (rootPath)) != NULL) { // acceppt only if path exists 
-        printf("pathEnd = %s\n", pathEnd);
         if(0 == currentLabel[0]) { // no currentLabel means end of path reached
-            strcpy(foundPath, rootPath);
-            printf("1. return: %s\n", foundPath);
+            strncpy(foundPath, rootPath, MAX_STR_LEN - 1);
             closedir (dir);
             return foundPath; // means the path is found in deepest recursion
         }
         while ((ent = readdir (dir)) != NULL) {  // go through all dirs in rootPath
             cmpResult = cmpStrWild(ent->d_name, currentLabel); // check if wildcard matches
-            printf(" next: %s\n", ent->d_name);
             if(cmpResult) {  // found match - next recursion step, process inner directory wildcards
-                strcpy(found, rootPath);
-                //strcat(found, "/");
-                strcat(found, ent->d_name);
-                strcat(found, "/");
-                strcat(found, pathEnd);
-                printf(" found = %s\n", found);
-                printf(" foundPath = %s\n", foundPath);
-                if(findPathWild(foundPath, found)) {
-                    //strcpy(foundPath, found);
-                    printf("2. return: %s\n", foundPath);
+                strncpy(found, rootPath, MAX_STR_LEN - 1);
+                strncat(found, ent->d_name, MAX_STR_LEN - strlen(found) - 1);
+                strncat(found, "/", MAX_STR_LEN - strlen(found) - 1);
+                strncat(found, pathEnd, MAX_STR_LEN - strlen(found) - 1);
+                if(findPathWild(foundPath, found) != NULL) {
                     closedir (dir);
                     return foundPath;
                 }
@@ -117,7 +98,6 @@ char* findPathWild(char* foundPath, char* wildPath)
         closedir (dir);
         return NULL;
     } else {
-        perror ("");
         return NULL;
     }
 }
@@ -125,9 +105,7 @@ char* findPathWild(char* foundPath, char* wildPath)
 
 
 int main(void) {
-    DIR *dir;
-    struct dirent *ent;
-    char path[512] = "";
+    char path[MAX_STR_LEN] = "";
     //char wild[] = "/usr/l??/nvidia-???/";
     //char wild[] = "/usr/???/nvidia-???";
     char wild[] = "/???/???/nvidia-???/";
@@ -135,33 +113,9 @@ int main(void) {
     //char wild[] = "/usr/lib/nvidia-375/";
     //char wild[] = "/usr/lib/nvidia-475/";
     //char wild[] = "/usr/lib/nvidia-???/";
-    char* pend;
-    char val[512];
-    int x;
-    //pend = getRootPath(path, wild); 
-    //printf(path);
-    //printf(pend);
 
 
     printf("found = %s\n", findPathWild(path, wild));
 
-    /*
-    if ((dir = opendir (path)) != NULL) {
-      while ((ent = readdir (dir)) != NULL) {
-        
-        //printf ("pend: %s\n", pend);
-        //printf ("name: %s\n", ent->d_name);
-        x = cmpStrWild(ent->d_name, pend);
-        //printf("%d\n", x);
-        if(x) printf("%s\n", ent->d_name);
-        //if(NULL != val){
-        //  printf("%s\n", val);
-        //}
-      }
-      closedir (dir);
-    } else {
-      perror ("");
-      return -1;
-    }
-*/
+    return 0;
 }
